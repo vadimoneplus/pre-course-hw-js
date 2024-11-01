770f4428af1b3de46360031a2b1fcae2f2feab94
vadimoneplus <v.maranchak@gmail.com>
2024-11-01 11:17:48 +0300

    lesson-02 task-1

--- lesson-01/06.js
+++ lesson-01/06.js
@@ -1,3 +1,4 @@
 let str = "zzzzyzz";
 let result =str.includes('y') ? 'нашел' : 'заблудился';
 console.log(result)
+
--- lesson-01/07.js
+++ lesson-01/07.js
@@ -3,3 +3,7 @@ let krasavchik = "красавчик!";
 
 console.log(iAm.toUpperCase() + ' ' + krasavchik);
 
+let a = [1, 2, 3]; 
+ delete a[2]; 
+ 
+console.log(null + undefined )
